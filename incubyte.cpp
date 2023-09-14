424fe1ae6d020fe29be0303c09b41c607654dc72
Pratik <prsonawane21@gmail.com>
2023-09-15 00:45:39 +0530

    Added code for ther test cases

--- incubyte.cpp
+++ incubyte.cpp
@@ -91,7 +91,7 @@ int main(){
     cin>>c;
 
     //Taking initial direction
-    char ch,temp;
+    char ch,current_direction,prev_direction;
     cout<<"Enter initial direction (from N,S,E,W,U,D) in uppercase as a single character : ";
     cin>>ch;
 
@@ -125,102 +125,149 @@ int main(){
         switch (c)
         {
         case 'u':
+            prev_direction=obj.get_direction();
             obj.set_direction('U');
             break;
 
         case 'd':
+            prev_direction=obj.get_direction();
             obj.set_direction('D');
             break;
 
         case 'l':
-            temp=obj.get_direction();
-            if(temp=='N'){
+            current_direction=obj.get_direction();
+            if(current_direction=='N'){
                 obj.set_direction('W');
             }
-            else if(temp=='E'){
+            else if(current_direction=='E'){
                 obj.set_direction('N');
             }
-            else if(temp=='S'){
+            else if(current_direction=='S'){
                 obj.set_direction('E');
             }
-            else if(temp=='W'){
+            else if(current_direction=='W'){
                 obj.set_direction('S');
             }
-            else if(temp=='U'){
-                obj.set_direction('N');
+            else if(current_direction=='U'){
+                if(prev_direction=='E'){
+                    obj.set_direction('N');
+                }
+                else if(prev_direction=='N'){
+                    obj.set_direction('W');
+                }
+                else if(prev_direction=='W'){
+                    obj.set_direction('S');
+                }
+                else if(prev_direction=='S'){
+                    obj.set_direction('E');
+                }
             }
-            else if(temp=='D'){
-                obj.set_direction('S');
+            else if(current_direction=='D'){
+                if(prev_direction=='E'){
+                    obj.set_direction('N');
+                }
+                else if(prev_direction=='N'){
+                    obj.set_direction('W');
+                }
+                else if(prev_direction=='W'){
+                    obj.set_direction('S');
+                }
+                else if(prev_direction=='S'){
+                    obj.set_direction('E');
+                }
             }
             break;
 
         case 'r':
 
-            temp=obj.get_direction();
-            if(temp=='N'){
+            current_direction=obj.get_direction();
+            if(current_direction=='N'){
                 obj.set_direction('E');
             }
-            else if(temp=='E'){
+            else if(current_direction=='E'){
                 obj.set_direction('S');
             }
-            else if(temp=='S'){
+            else if(current_direction=='S'){
                 obj.set_direction('W');
             }
-            else if(temp=='W'){
+            else if(current_direction=='W'){
                 obj.set_direction('N');
             }
-            else if(temp=='U'){
-                obj.set_direction('S');
+            else if(current_direction=='U'){
+                if(prev_direction=='E'){
+                    obj.set_direction('S');
+                }
+                else if(prev_direction=='N'){
+                    obj.set_direction('E');
+                }
+                else if(prev_direction=='W'){
+                    obj.set_direction('N');
+                }
+                else if(prev_direction=='S'){
+                    obj.set_direction('W');
+                }
             }
-            else if(temp=='D'){
-                obj.set_direction('N');
+            else if(current_direction=='D'){
+
+                if(prev_direction=='E'){
+                    obj.set_direction('S');
+                }
+                else if(prev_direction=='N'){
+                    obj.set_direction('E');
+                }
+                else if(prev_direction=='W'){
+                    obj.set_direction('N');
+                }
+                else if(prev_direction=='S'){
+                    obj.set_direction('W');
+                }
             }
             break;
 
         case 'f':
 
-            temp=obj.get_direction();
+            current_direction=obj.get_direction();
 
-            if(temp=='N'){
+            if(current_direction=='N'){
                 obj.add_to_y(1);
             }
-            else if(temp=='E'){
+            else if(current_direction=='E'){
                 obj.add_to_x(1);
             }
-            else if(temp=='S'){
+            else if(current_direction=='S'){
                 obj.add_to_y(-1);
             }
-            else if(temp=='W'){
+            else if(current_direction=='W'){
                 obj.add_to_x(-1);
             }
-            else if(temp=='U'){
+            else if(current_direction=='U'){
                 obj.add_to_z(1);
             }
-            else if(temp=='D'){
+            else if(current_direction=='D'){
                 obj.add_to_z(-1);
             }
 
             break;
 
         case 'b':
-            temp=obj.get_direction();
+            current_direction=obj.get_direction();
 
-            if(temp=='N'){
+            if(current_direction=='N'){
                 obj.add_to_y(-1);
             }
-            else if(temp=='E'){
+            else if(current_direction=='E'){
                 obj.add_to_x(-1);
             }
-            else if(temp=='S'){
+            else if(current_direction=='S'){
                 obj.add_to_y(1);
             }
-            else if(temp=='W'){
+            else if(current_direction=='W'){
                 obj.add_to_x(1);
             }
-            else if(temp=='U'){
+            else if(current_direction=='U'){
                 obj.add_to_z(-1);
             }
-            else if(temp=='D'){
+            else if(current_direction=='D'){
                 obj.add_to_z(1);
             }
 
@@ -254,12 +301,12 @@ int main(){
 
 
     *************************************INPUT1****************************************
-    PS D:\DS\Placements\Codes\Incubyte> cd "d:\DS\Placements\Codes\Incubyte\\incubyte }
+    PS D:\DS\Placements\Codes\Incubyte> cd "d:\DS\Placements\Codes\Incubyte\" ; if ($?) { g++ incubyte.cpp -o incubyte } ; if ($?) { .\incubyte }
     Enter initial coordinates(x,y,z)
     Enter x coordinate : 0
     Enter y coordinate : 0
     Enter z coordinate : 0
-    Enter initial direction : N
+    Enter initial direction (from N,S,E,W,U,D) in uppercase as a single character : N
     Enter commnads and 0 to end taking commnads : 
     f
     r
@@ -292,7 +339,6 @@ int main(){
 
     After Command "b" configuration is :
     Coordinates : (0,1,-1)
-    Direction : U
 
 
 
@@ -307,15 +353,12 @@ int main(){
 
     Coordinates : (0,1,-1)
     Direction : N
-
-
-    ***************************************INPUT 2**************************************
-    PS D:\DS\Placements\Codes\Incubyte> cd "d:\DS\Placements\Codes\Incubyte\\incubyte }
+    PS D:\DS\Placements\Codes\Incubyte> cd "d:\DS\Placements\Codes\Incubyte\" ; if ($?) { g++ incubyte.cpp -o incubyte } ; if ($?) { .\incubyte }
     Enter initial coordinates(x,y,z)
     Enter x coordinate : -1
     Enter y coordinate : -1
     Enter z coordinate : -1
-    Enter initial direction : S
+    Enter initial direction (from N,S,E,W,U,D) in uppercase as a single character : S
     Enter commnads and 0 to end taking commnads : 
     f
     l
@@ -356,6 +399,7 @@ int main(){
 
     After Command "b" configuration is :
     Coordinates : (-1,-2,-2)
+    Direction : U
 
 
 
